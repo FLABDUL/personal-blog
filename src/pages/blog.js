9a0536fdb765f500ac8005b9c1e92e75e832fc94
Abdul Hakim Norazman <ahnorazman@gmail.com>
2025-06-16 19:52:24 +0100

    added external blogs

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -36,6 +36,16 @@ export default function BlogPage({ data }) {
       title: "Greenhorn Software Engineer",
       date: "March 10, 2024",
       link: "https://www.linkedin.com/pulse/greenhorn-software-engineer-abdul-hakim-norazman"
+    },
+    {
+      title: "Meet the Makers – University of Edinburgh Bulletin",
+      date: "November 2, 2021",
+      link: "https://bulletin.ed.ac.uk/2021/11/02/meet-the-makers/"
+    },
+    {
+      title: "Engineering Alumni Profile – Abdul Hakim Norazman",
+      date: "October 2021",
+      link: "https://employ.eng.ed.ac.uk/alumni-profiles/abdul-hakim-norazman-jp-morgan-chase-co"
     }
   ];
 
@@ -56,16 +66,17 @@ export default function BlogPage({ data }) {
         </div>
       ))}
 
-      {/* External LinkedIn Blog Posts */}
+      {/* External Posts */}
+      <h2 style={{ marginTop: "4rem" }}>External Features & Posts</h2>
       {externalPosts.map((post, idx) => (
         <div key={`external-${idx}`} style={{ marginBottom: "2rem" }}>
-          <h2>
+          <h3>
             <a href={post.link} target="_blank" rel="noopener noreferrer" style={{ color: "#4fc3f7" }}>
               {post.title}
             </a>
-          </h2>
+          </h3>
           <small>{post.date}</small>
-          <p>Read on LinkedIn →</p>
+          <p>Read more →</p>
         </div>
       ))}
     </Layout>
